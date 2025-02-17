54424ead2a49ded195cbf5858dba2838b1871063
AjayJadhav2151 <ajayjadhav.skn.comp@gmail.com>
2025-02-17 22:35:21 +0530

    adding second Assignment

---
+++ ass2.cpp
@@ -0,0 +1,9 @@
+// Accept a number from user and display it on the screen.
+#define _CRT_SECURE_NO_WARNINGS
+ #include<iostream>
+ int main(){
+     int a , b;
+     printf("Enter the 2 no.");
+     scanf("%d%d", &a, &b);
+     printf("%d \n%d", a, b);
+ }
